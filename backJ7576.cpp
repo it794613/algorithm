0ddbb408ae54b8b90332d5b9011d163362aebcbf
it794613 <it794613@naver.com>
2022-09-18 19:41:20 +0900

    backJ7576

--- backJ7576.cpp
+++ backJ7576.cpp
@@ -1,97 +1,75 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 #include <algorithm>
+#include <queue>
 
-#define MAX 101
+#define MAX 1001
 
 using namespace std;
 
-bool visited[MAX][MAX];
-int map[MAX][MAX] = {
-    0,
-};
-int n, m;
-queue<pair<int, int>> q;
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, -1, 1};
+typedef struct coord{
+    int x, y;
+}coord;
 
-void outmap()
-{
-    for (int a = 0; a < m; a++)
-    {
-        cout << "\n";
-        for (int b = 0; b < n; b++)
-        {
-            cout << map[a][b];
-        }
-    }
-    cout << "\n";
-}
+int map[MAX][MAX] = {0,};
+bool visited[MAX][MAX] = {0,};
+int x,y;
+queue<coord> q;
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
 
-void bfs()
-{
-    while (!q.empty())
-    {
-        int x = q.front().second;
-        int y = q.front().first;
-        visited[y][x] = true;
+void bfs(){
+    while(!q.empty()){
+        coord temp = q.front();
         q.pop();
-        for (int a = 0; a < 4; a++)
-        {
-            int next_y = y + dy[a];
-            int next_x = x + dx[a];
-            if (next_y >= 0 && next_y < m && next_x >= 0 && next_x < n && visited[next_y][next_x] == false && map[next_y][next_x] == 0)
-            {
-                map[next_y][next_x] = map[y][x] + 1;
-                q.push(make_pair(next_y, next_x));
-                visited[next_y][next_x] = true;
+        for(int a=0;a<4;a++){
+            coord next;
+            next.x = temp.x + dx[a];
+            next.y = temp.y + dy[a];
+            if(next.y >=0 && next.y < y && next.x >= 0 && next.x < x && map[next.y][next.x]== 0 && visited[next.y][next.x]==false){
+                q.push(next);
+                visited[next.y][next.x]= true;
+                map[next.y][next.x] = map[temp.y][temp.x] + 1;
             }
         }
-        outmap();
     }
 }
 
-int main()
-{
-    cin >> n >> m;
-    for (int a = 0; a < m; a++)
-    {
-        for (int b = 0; b < n; b++)
-        {
+int main(){
+    cin >> x >> y;
+    int maxNumber = -1;
+    int result = 0;
+    for(int a=0;a<y;a++){
+        for (int b=0;b<x;b++){
             cin >> map[a][b];
         }
     }
-    for (int a = 0; a < n; a++)
-    {
-        for (int b = 0; b < m; b++)
-        {
-            if (visited[a][b] == false && map[a][b] == 1)
-            {
-                int x = b;
-                int y = a;
-                q.push(make_pair(y, x));
+    for(int a=0;a<y;a++){
+        for(int b=0;b<x;b++){
+            if(visited[a][b]== false && map[a][b]==1){
+                coord start;
+                start.y = a;
+                start.x = b;
+                q.push(start);
             }
         }
     }
     bfs();
-    int max = 0;
-    for (int a = 0; a < m; a++)
-    {
-        for (int b = 0; b < n; b++)
-        {
-            if (map[a][b] == 0)
-            {
-                cout << -1;
+    for(int a=0;a<y;a++){
+        for(int b=0;b<x;b++){
+            if(map[a][b] == 0){
+                result = 1;
             }
-            if (map[a][b] != 0 && map[a][b] != -1)
-            {
-                if (max < map[a][b])
-                {
-                    max = map[a][b];
+            else{
+                if(maxNumber < map[a][b]){
+                    maxNumber = map[a][b];
                 }
             }
         }
     }
-    cout << max;
-    return 0;
+    if(result == 1){
+        cout<< -1;
+    }else{
+        cout<< maxNumber - 1;
+    }
 }
